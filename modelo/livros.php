506b2ed907b76f047bdbfe84ab1528fd1c385649
PEDRO AUGUSTO SANTOS ALMEIDA <pedro.asalmeida1@senacsp.edu.br>
2024-07-30 21:58:01 -0300

    fix: add livros

--- modelo/livros.php
+++ modelo/livros.php
@@ -2,36 +2,33 @@
     namespace php\modelo;
 
     class Livros{
-        public Boolean Validar(int $login)
-        {
-            if ($login == 0)
-            {
-                return false;
-            }
-            else
-            { 
-                return true;    
-            }
-        }
-
         public int aVoltaDosQueNaoForam(){
-            return 333;
+            return "333 livros";        
         }
 
         public int dicasDeComoChegarla(){
-            return 269;
+            return "269";
         }
 
         public int logicaDeProgramacao(){
-            return 100;
+            return "100";
         }
 
         public int redesDeComputadores(){
-            return 53;
+            return "53";
         }
 
         public int harryPotter(){
-            return 20;
+            return "20";
         }
+
+        public function livros(){
+            return "<br> Estoque dos Livros:".
+                       "<br>A Volta Dos Que Não Foram: ".$this->aVoltaDosQueNaoForam().
+                       "<br>Dicas De Como Chegarla: ".$this->dicasDeComoChegarla().
+                       "<br>Lógica De Programacao: ".$this->logicaDeProgramacao().
+                       "<br>Redes De Computadores: ".$this->redesDeComputadores().
+                       "<br>Harry Potter: ".$this->harryPotter();
+            }
     }
 ?>
--- modelo/main.php
+++ modelo/main.php
@@ -1,10 +1,15 @@
 <?php
     namespace php\modelo;//Definir o local do projeto
     include 'usuario.php';
+    include 'livros.php';
 
     $usuario = new Usuario("Pedro","Rua João","11999999999","26/06/2001","root@root.com","*******");//Instanciando
+
+    $livros = new Livros("333","269","100","53","20");
     
     echo $usuario -> login();
     echo "<br>";
     echo $usuario -> cadastro();
+    echo "<br>";
+    echo $livros -> livros();
 ?>
